38cfba4e1bc2ad56c43ca4c0770d139eef23452b
fr3ooon1 <fero>
2024-08-04 13:57:43 +0300

    fix

--- elaguiely/apis/item.py
+++ elaguiely/apis/item.py
@@ -141,6 +141,5 @@ def get_items (filters = {} ,*args , **kwargs) :
    for item in items:
        item['image'] = url + item.get('image')
        item["after_discount"] = item.get('item_price') - item.get('item_discount')
-    #    fg
    return items
 
