5339b0edce3c47d9042cc8624becd48dc135c519
Ameer Abdulaziz <ameer.abdulaziz93@gmail.comn>
2024-09-30 20:35:11 +0300

    Ignore permissions when listing govs, cities, and zones

--- elaguiely/ts_api.py
+++ elaguiely/ts_api.py
@@ -8,7 +8,7 @@ import json
 def get_all_zones():
     try:
         response = []
-        governs = frappe.db.get_list("Zone", fields= ['zone_code', 'zone_name', 'city_code', 'gov_code'])
+        governs = frappe.db.get_list("Zone", fields= ['zone_code', 'zone_name', 'city_code', 'gov_code'], ignore_permissions=True)
         for gov in governs:
             response.append({
                 "Z_code": gov.get('zone_code'), 
@@ -29,7 +29,7 @@ def get_all_zones():
 def get_all_cities():
     try:
         response = []
-        governs = frappe.db.get_list("City", fields= ['city_code', 'city_name', 'gov_code'])
+        governs = frappe.db.get_list("City", fields= ['city_code', 'city_name', 'gov_code'], ignore_permissions=True)
         for gov in governs:
             response.append({
                 "City_code": gov.get('city_code'), 
@@ -47,7 +47,7 @@ def get_all_governs():
     print('get_all_governs')
     try:
         response = []
-        governs = frappe.db.get_list("Governorate", fields= ['gov_code', 'gov_name'])
+        governs = frappe.db.get_list("Governorate", fields= ['gov_code', 'gov_name'], ignore_permissions=True)
         for gov in governs:
             response.append({
                 "Gov_code": gov.get('gov_code'), 
