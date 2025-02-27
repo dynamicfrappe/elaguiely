ef0fea2bacdb725440f4f97d07ac17f066086ef0
dynamicTech123 <sabdelhameed@gmail.com>
2025-02-27 17:48:45 +0200

    update item unit selection

---
+++ elaguiely/controllers/queries.py
@@ -0,0 +1,35 @@
+import frappe
+from frappe.model.document import Document
+from collections import defaultdict
+from frappe.desk.reportview import get_filters_cond, get_match_cond
+
+@frappe.whitelist()
+def warehouse_query(doctype, txt, searchfield, start, page_len, filters):
+	# Should be used when item code is passed in filters.
+	doctype = "UOM Conversion Detail"
+	conditions, bin_conditions = [], []
+	filter_dict = get_doctype_wise_filters(filters)
+	
+	query = """select uom
+		from `tabUOM Conversion Detail`
+		where 1=1 {fcond} order by name
+		limit
+			{start}, {page_len}
+		""".format(		
+		fcond=get_filters_cond(doctype, filter_dict.get("parent"), conditions),		
+		start=start,
+		page_len=page_len,
+		txt=frappe.db.escape("%{0}%".format(txt)),
+	) 
+	
+	return frappe.db.sql(query)
+
+
+def get_doctype_wise_filters(filters):
+	# Helper function to seperate filters doctype_wise
+	filter_dict = defaultdict(list)
+	for row in filters:
+		filter_dict[row[0]].append(row)
+	return filter_dict
+
+
--- elaguiely/public/js/sales_order.js
+++ elaguiely/public/js/sales_order.js
@@ -1,13 +1,62 @@
-frappe.ui.form.on('Sales Order Item' , {
+frappe.ui.form.on("Sales Order", {
+	
 
-    refressh:function(frm){
+    onload: function(frm) {
+		
+		frm.set_query('uom', 'items', function(doc, cdt, cdn) {
+			let row  = locals[cdt][cdn];
+			let query = {
+				filters: [
+				]
+			};
+			if (row.item_code) {
+				query.query = "elaguiely.controllers.queries.warehouse_query";
+				query.filters.push(["parent", "=", row.item_code]);
+			}
+			return query;
+		});
 
-        window.onload = function() {
-            if (localStorage.getItem("isReadOnly") === "true") {
-              document.getElementById("price_list_rate").readOnly = true;
-            }
-        };
         
-    },
+	},
+});
 
-})
+// frappe.ui.form.on("Sales Order Item", {
+   
+//     item_code: function (frm, cdt, cdn) {
+         
+//         let row = locals[cdt][cdn];
+//         console.log(row);
+//         frappe.db.get_list('UOM Conversion Detail', {
+//             fields: ['uom', 'conversion_factor'],
+//             filters: {
+//                 parent: row.item_code
+//             }
+//         }).then(records => {
+
+//             let uoms = records.map(item => item.uom);
+//             console.log(uoms);
+//             frm.fields_dict.items.grid.get_field('uom').get_query =function(){
+//                 return {
+//                                 filters: {
+//                                     name: ["in", uoms]
+//                                 }
+//                             }
+//             }
+//            
+
+//             //frm.refresh_fields('items');
+//             // if (row.item_code) {
+//             //     frm.set_query('uom', 'items', () => {
+//             //         return {
+//             //             filters: {
+//             //                 name: ["in", uoms]
+//             //             }
+//             //         }
+//             //     })
+                
+//             // }
+//         })
+        
+      
+//     }
+// });
