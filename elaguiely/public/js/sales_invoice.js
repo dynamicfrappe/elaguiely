e47e87a1aadcc5527467e1c3f36c1cc7b7a89aff
noha3011 <eng.noha.30.11@gmail.com>
2025-03-16 13:54:54 +0200

    update maxqty condition

--- elaguiely/public/js/sales_invoice.js
+++ elaguiely/public/js/sales_invoice.js
@@ -28,6 +28,7 @@ frappe.ui.form.on('Sales Invoice', {
     },
 
     onload: function(frm) {
+       
 		
 		frm.set_query('uom', 'items', function(doc, cdt, cdn) {
 			let row  = locals[cdt][cdn];
@@ -38,6 +39,7 @@ frappe.ui.form.on('Sales Invoice', {
 			if (row.item_code) {
 				query.query = "elaguiely.controllers.queries.uom_query";
 				query.filters.push(["parent", "=", row.item_code]);
+                
 			}
 			return query;
 		});
